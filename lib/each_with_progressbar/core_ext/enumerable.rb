99bcbbdaab67f187ed038a0347650a183e763a21
Manbo- <Manbo-@server.fake>
2014-12-08 11:48:18 +0900

    return Enumerator

--- lib/each_with_progressbar/core_ext/enumerable.rb
+++ lib/each_with_progressbar/core_ext/enumerable.rb
@@ -2,6 +2,7 @@ require 'ruby-progressbar'
 
 module Enumerable
   def each_with_progressbar(options = { format: "%t: |%B| %p%% %c/%C %E" })
+    return to_enum(:each_with_progressbar) unless block_given?
     options.merge!(total: length) if respond_to?(:length)
     progressbar = ProgressBar.create(options)
     each do |blk|
